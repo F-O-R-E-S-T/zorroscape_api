85bcd4b0388a01b99442dad4c522f9570fdb8ff3
Andres M. Prieto <andresprieto2703@gmail.com>
2023-07-10 01:36:33 -0500

    Adding files
    Using config and FileServe to handle with attack

--- services/attack.service.js
+++ services/attack.service.js
@@ -1,11 +1,38 @@
-const FileService = require("./file.service.js");
+const FileService = require("./file.service");
+const { config } = require("../config");
 
 class AttackService extends FileService {
   constructor() {
     super();
+    this.speed = config.attackSpeed;
+    this.distance = config.attackDistance;
   }
-  setRight() {}
-  setLeft() {}
+
+  async setRight() {
+    let current = await this.getFileData();
+
+    const update = {
+      ...current[0],
+    };
+    update.data.x = current[0].data.x + this.speed;
+    update.data.attacking = current[0].data.attacking + this.distance;
+
+    await this.updateFile(update.fileName, update.data);
+    return update.data;
+  }
+
+  async setLeft() {
+    let current = await this.getFileData();
+
+    const update = {
+      ...current[0],
+    };
+    update.data.x = current[0].data.x - this.speed;
+    update.data.attacking = current[0].data.attacking - this.distance;
+
+    await this.updateFile(update.fileName, update.data);
+    return update.data;
+  }
 }
 
 module.exports = new AttackService();
