f2dbdcd483ada2f35e11344f4a75411ebed37a00
Andres M. Prieto <andresprieto2703@gmail.com>
2023-07-10 01:33:37 -0500

    Extending config
    Adding previous vars to config

--- config/index.js
+++ config/index.js
@@ -1,17 +1,25 @@
-require('dotenv').config();
+require("dotenv").config();
 
 const config = {
-  env: process.env.NODE_ENV || 'development',
+  env: process.env.NODE_ENV || "development",
 
   port: process.env.PORT || 3000,
 
-  appHost: process.env.APP_HOST || 'localhost',
-  appRoute: process.env.APP_ROUTE || '/api/v1',
+  appHost: process.env.APP_HOST || "localhost",
+  appRoute: process.env.APP_ROUTE || "/api/v1",
 
-  appPublic: process.env.APP_PUBLIC || '/',
-  appStatic: process.env.APP_STATIC || 'public',
+  appPublic: process.env.APP_PUBLIC || "/",
+  appStatic: process.env.APP_STATIC || "public",
 
-  appStatusFilePath: process.env.APP_FILES || 'files',
-}
+  appStatusFilePath: process.env.APP_FILES || "files",
+
+  movementSpeed: parseInt(process.env.MOVEMENT_SPEED) || 10,
+
+  jumpingSpeed: parseInt(process.env.JUMPING_SPEED) || 10,
+  jumpingHigh: parseInt(process.env.JUMPING_HIGH) || 10,
+
+  attackDistance: parseInt(process.env.ATTACK_DISTANCE) || 10,
+  attackSpeed: parseInt(process.env.ATTACK_SPEED) || 10,
+};
 
 module.exports = { config };
