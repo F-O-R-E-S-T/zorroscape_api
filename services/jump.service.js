f21f7a460b6b979bd94868c73fd493bc2d2c861a
Andres M. Prieto <andresprieto2703@gmail.com>
2023-07-10 01:37:15 -0500

    Jumping service
    Adding jumping service configuration

--- services/jump.service.js
+++ services/jump.service.js
@@ -1,11 +1,40 @@
-const FileService = require("./file.service.js");
+const FileService = require("./file.service");
+const { config } = require("../config");
 
 class JumpService extends FileService {
   constructor() {
     super();
+    this.high = config.jumpingHigh;
+    this.speed = config.jumpingSpeed;
   }
-  setRight() {}
-  setLeft() {}
+
+  async setRight() {
+    let current = await this.getFileData();
+
+    const update = {
+      ...current[0],
+    };
+    update.data.y = current[0].data.y + this.high;
+    update.data.x = current[0].data.x + this.speed;
+    update.data.jumping = !current[0].data.jumping;
+
+    await this.updateFile(update.fileName, update.data);
+    return update.data;
+  }
+
+  async setLeft() {
+    let current = await this.getFileData();
+
+    const update = {
+      ...current[0],
+    };
+    update.data.y = current[0].data.y + this.high;
+    update.data.x = current[0].data.x - this.speed;
+    update.data.jumping = !current[0].data.jumping;
+
+    await this.updateFile(update.fileName, update.data);
+    return update.data;
+  }
 }
 
 module.exports = new JumpService();
