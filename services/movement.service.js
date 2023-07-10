355d698c5be1ceb5fc7fc7f4416ec394a30fcdbe
Andres M. Prieto <andresprieto2703@gmail.com>
2023-07-10 01:36:07 -0500

    Config vars
    Using variables from config

--- services/movement.service.js
+++ services/movement.service.js
@@ -1,16 +1,19 @@
 const FileService = require("./file.service");
+const { config } = require("../config");
 
 class MovementService extends FileService {
   constructor() {
     super();
+    this.speed = config.movementSpeed;
   }
+
   async setRight() {
     let current = await this.getFileData();
 
     const update = {
       ...current[0],
     };
-    update.data.x = current[0].data.x + 10;
+    update.data.x = current[0].data.x + this.speed;
 
     await this.updateFile(update.fileName, update.data);
     return update.data;
@@ -22,7 +25,7 @@ class MovementService extends FileService {
     const update = {
       ...current[0],
     };
-    update.data.x = current[0].data.x - 10;
+    update.data.x = current[0].data.x - this.speed;
 
     await this.updateFile(update.fileName, update.data);
     return update.data;
@@ -34,7 +37,7 @@ class MovementService extends FileService {
     const update = {
       ...current[0],
     };
-    update.data.y = current[0].data.y - 10;
+    update.data.y = current[0].data.y - this.speed;
 
     await this.updateFile(update.fileName, update.data);
     return update.data;
@@ -46,7 +49,7 @@ class MovementService extends FileService {
     const update = {
       ...current[0],
     };
-    update.data.y = current[0].data.y + 10;
+    update.data.y = current[0].data.y + this.speed;
 
     await this.updateFile(update.fileName, update.data);
     return update.data;
