7b8e2db817c4b2affb405bfd66aadd961a6e42de
Andres M. Prieto <andresprieto2703@gmail.com>
2023-07-10 03:09:38 -0500

    Idle structure
    Adding functions to handle with file creation (Die, Start) also a getter
    (getCurrent), still missing win conditions

--- services/idle.service.js
+++ services/idle.service.js
@@ -1,13 +1,38 @@
-const FileService = require("./file.service.js");
+const FileService = require("./file.service");
 
 class IdleService extends FileService {
   constructor() {
     super();
+    this.template = {
+      x: 0,
+      y: 0,
+      attacking: 0,
+      jumping: false,
+    };
   }
-  getCurrent() {}
-  win() {}
-  start() {}
-  die() {}
+
+  async getCurrent() {
+    const gameStatus = await this.getFileData();
+    return gameStatus[0].data;
+  }
+
+  win() {
+    /**
+     TO DO
+
+    Ask for win conditions
+    */
+  }
+
+  async start() {
+    await this.updateFile("gameStatus.json", this.template);
+  }
+
+  async die() {
+    let gameFile = await this.getFileData();
+    const isDeath = await this.deleteRef(gameFile[0].fileName);
+    return isDeath;
+  }
 }
 
 module.exports = new IdleService();
