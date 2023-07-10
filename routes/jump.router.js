d01336a79f66c0714fd2ce0c31019565571f6dcb
Andres M. Prieto <andresprieto2703@gmail.com>
2023-07-10 01:35:32 -0500

    Quick fix
    Adding await keyword

--- routes/jump.router.js
+++ routes/jump.router.js
@@ -2,18 +2,18 @@ const jumpService = require("../services/jump.service");
 const router = require("express").Router();
 const { config } = require("../config");
 
-router.post("/right", async (req, res) => {
+router.put("/right", async (req, res) => {
   try {
-    const status = jumpService.setRight();
+    const status = await jumpService.setRight();
     res.status(200).json(status);
   } catch (error) {
     console.error(error);
   }
 });
 
-router.post("/left", async (req, res) => {
+router.put("/left", async (req, res) => {
   try {
-    const status = jumpService.setLeft();
+    const status = await jumpService.setLeft();
     res.status(200).json(status);
   } catch (error) {
     console.error(error);
